a778903936e17fdd7a63baf3b8677df1e53b57f6
Zachary Schreiber <zachgives@gmail.com>
2016-10-23 11:53:36 -0400

    uuuuhhhhh.....

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,131 +1,75 @@
-(function($) {
-  //On pageload instructions pop up
-  $(function(){
-    $('#game-instructions').hide().fadeIn(2500);
-    $('#flying-pot').hide();
-  });
+//On pageload instructions pop up
+$(function(){
+  $('#game-instructions').hide().fadeIn(2500);
+  $('#flying-pot').hide();
+});
+//make them go away
+$('#close').click(function(){
+  $('#game-instructions').fadeOut().hide(2000);
+});
+//Login/Signup listeners
+$('#login').click(function(event) {
+    event.preventDefault();
+    // new User(loginValues());
+    loginValues();
+});
 
-  //make them go away
-  $('#close').click(function(){
-    $('#game-instructions').fadeOut().hide(2000);
-  });
+$('#signup').click(function(event) {
+    event.preventDefault();
+    signUpValues();
+});
+//object to pass into ajax
+function loginValues() {
+    var context = {
+        username: $('#username').val(),
+        password: $('#password').val(),
+    };
+    return context;
+}
+//GETs user database and iterates over it
+function checkExisting(context) {
+    $.ajax({
+        "method": "GET",
+        "url": '/login/users' + context,
+        "data": {},
+        "datatype": "json",
+        "success": function(data) {
+            for (var index = 0; index.length; index++) {
+                if (data.username == context.username)
+                    if (data.password == context.password) {
+                        window.location.replace("/game");
+                    } else {
+                        window.location.replace("/login");
+                    }
+            }
 
-  //Login/Signup listeners
-  $('#login').click(function(event) {
-      event.preventDefault();
-      // new User(loginValues());
-      loginValues();
-  });
-
-  $('#signup').click(function(event) {
-      event.preventDefault();
-      signUpValues();
-  });
-  //object to pass into ajax
-  function loginValues() {
-      var context = {
-          username: $('#username').val(),
-          password: $('#password').val(),
-      };
-      return context;
-  }
-  //GETs user database and iterates over it
-  function checkExisting(context) {
-      $.ajax({
-          "method": "GET",
-          "url": '/login/users' + context,
-          "data": {},
-          "datatype": "json",
-          "success": function(data) {
-              for (var index = 0; index.length; index++) {
-                  if (data.username == context.username)
-                      if (data.password == context.password) {
-                          window.location.replace("/game");
-                      } else {
-                          window.location.replace("/login");
-                      }
-              }
-          }
-      });
-  }
-  //Random constructor, Make use of me
-  function UserData(userObj) {
-      this.info = {
-          username: userObj.username,
-          password: userObj.password,
-          totalpoints: userObj.total_points,
-
-      };
-  }
-
-
-  function signUpValues() {
-      var newContext = {
-          username: $('#username').val(),
-          password: $('#password').val(),
-          confirm: $('#confirm').val()
-      };
-      if (newContext.password === newContext.confirm) {
-          checkUsername(newContext);
-      } else {
-          alert("Passwords do not match");
-      }
-
-  }
-  /**************************************
-  Mr. Potato Who Game
-  **************************************/
-  function PotatoGame() {
-    this.pageNav();
-    this.potatoBoard();
-  }
-  /**************************************
-  When users click on prop, it will appear on potato
-  **************************************/
-  PotatoGame.prototype.potatoBoard = function() {
-    $('.prop').on('click', 'img', function() {
-      var image = $(this).attr("src");
-
-      if(image === '/app/assets/images/glasses.svg') {
-        $('.propFeatures .glasses').show();
-      } else if(image === '/app/assets/images/hat.svg') {
-        $('.propFeatures .hat').show();
-      } else if(image === '/app/assets/images/bow-tie.svg') {
-        $('.propFeatures .bow-tie').show();
-      } else if(image === '/app/assets/images/mustache.svg') {
-        $('.propFeatures .mustache').show();
-      } else if(image === '/app/assets/images/tattoo.svg') {
-        $('.propFeatures .tattoo').show();
-      } else if(image === '/app/assets/images/fork.svg') {
-        $('.propFeatures .fork').show();
-      }
+        }
     });
-  };
+}
+//Random constructor, Make use of me
+function UserData(userObj) {
+    this.info = {
+        username: userObj.username,
+        password: userObj.password,
+        totalpoints: userObj.total_points,
 
-  /**************************************
-  Click to Pages
-  **************************************/
-  PotatoGame.prototype.pageNav = function() {
-
-    this.leaderboard = function() {
-      $('.leaderpage').on('click', function() {
-        location.href = "leaderboard.html";
-      });
     };
-    this.leaderboard();
+}
 
-    this.game = function() {
-      $('.logo').on('click', function() {
-        location.href = "game.html";
-      });
+
+function signUpValues() {
+    var newContext = {
+        username: $('#username').val(),
+        password: $('#password').val(),
+        confirm: $('#confirm').val()
     };
-    this.game();
+    if (newContext.password === newContext.confirm) {
+        checkUsername(newContext);
+    } else {
+        alert("Passwords do not match");
+    }
 
-  };
-
-  new PotatoGame();
-
-})(jQuery);
+}
 
 function checkUsername(newContext) {
   $.ajax({
@@ -183,6 +127,15 @@ function goodTimes(){
     tater($("#flying-pot").show(), 5000);
 }
 
+//Game Logic
+
+
+
+
+
+
+
+//
 //    function User(context) {
 //      this.info = {
 //       username: context.username,
@@ -211,3 +164,5 @@ function goodTimes(){
 //   } );
 //
 //   // reset the game
+//
+//
